98f34e7c9f30f9f954f7ff0f9d2f3cd9004063bd
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-01-05 20:00:21 +0100

    Added utils#_ensureDir

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,16 @@
 module.exports = {
   json: new (require('./JSONReader.js'))(),
+  _ensureDir: path => {
+    const fs = require('fs');
+    return new Promise((resolve, reject) => {
+      fs.mkdir(path, function(err) {
+        if (err) {
+          if (err.code === 'EEXIST') resolve();
+          else reject(err);
+        }
+        else resolve();
+      });
+    });
+  },
   sleep: ms => new Promise(resolve => setTimeout(resolve, ms))
 }
