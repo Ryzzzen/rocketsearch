0cb69be817d8652a2b8d7df79d1e5fc00ee2f951
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-01-06 10:55:41 +0100

    MagnetHandler improvements

--- postprocessing/MagnetHandler.js
+++ postprocessing/MagnetHandler.js
@@ -2,12 +2,12 @@ const magnet = require('magnet-uri');
 
 class MagnetHandler {
 
-   handle(doc) {
-    console.log('=> Checking magnet:', doc.links.magnet);
+  handle(doc) {
+    console.log('=> Checking magnet:', doc.meta.infoHash);
 
     const parsed = magnet.decode(doc.links.magnet);
 
-    doc.id = parsed.infoHash || doc.links.web[0];
+    doc.id = parsed.infoHash || doc.meta.infoHash || doc.links.web[0];
     doc.title = doc.title || parsed.name;
 
     return doc;
