9e74a2ce0e6afc1f816392e7c18a59f31c453588
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-01-06 11:54:50 +0100

    OpenBay fixes

--- postprocessing/importproviders/OpenBay.js
+++ postprocessing/importproviders/OpenBay.js
@@ -3,6 +3,7 @@ const fs = require('fs'), path = require('path');
 const categories = {
   anime: 500400,
   softwares: 100000,
+  software: 100000,
   games: 110000,
   adult: 500300,
   movies: 500200,
@@ -24,13 +25,13 @@ module.exports = function(filePath, handle) {
 
     console.log(`=> Reading ${d[0]} (${d[2]}) from OpenBay archive`);
 
-    let matches = /"(.*?)"/g.exec(d[0]), title = matches !== null ? matches[1] : d[0];
+    let matches = /"(.*?)"/g.exec(d[0]), title = matches !== null && matches.length === 2 ? matches[1] : d[0];
     data.push({
       title: title,
       id: d[2],
       links: {
         download: [],
-        magnet: `magnet:?xt=urn:btih:${d[2]}&dn=${encodeURIComponent(title.replace(/\"/g, ''))}&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969`,
+        magnet: `magnet:?xt=urn:btih:${d[2]}&dn=${encodeURIComponent(title)}&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopentor.org%3A2710&tr=udp%3A%2F%2Ftracker.ccc.de%3A80&tr=udp%3A%2F%2Ftracker.blackunicorn.xyz%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969`,
         web: []
       },
       categories: [categories[d[4].replace(/\"/g, '')] || d[4].replace(/\"/g, '')],
