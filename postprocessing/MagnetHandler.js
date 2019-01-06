159e2666b7d00d0706cbb29ad94f706d430829ab
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-01-06 07:38:58 +0100

    MagnetHandler: added support for title

--- postprocessing/MagnetHandler.js
+++ postprocessing/MagnetHandler.js
@@ -6,7 +6,10 @@ class MagnetHandler {
     console.log('=> Checking magnet:', doc.links.magnet);
 
     const parsed = magnet.decode(doc.links.magnet);
+
     doc.id = parsed.infoHash || doc.links.web[0];
+
+    if (!doc.title) doc.title = parsed.name;
 
     return doc;
   }
